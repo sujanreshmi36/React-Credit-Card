3ea9bbcfe0797a2e4d7f209d01059c36c0c6d4f1
sujanreshmi36 <sujan.reshmi.7@gmail.com>
2025-01-09 15:32:03 +0545

    completed credit card

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,71 @@
 import CreditCard from "./components/CreditCard";
 import "./App.css";
+import { useState } from "react";
 
 function App() {
-  function handleSubmit() {
-    console.log("submitted");
+  const currentMonth = new Date().getMonth() + 1;
+  const formatting = String(currentMonth).length <= 1 ? "0" : null;
+  const formattedCurrentMonth = formatting + currentMonth;
+  const currentYear = new Date().getFullYear();
+  const [formData, setFormData] = useState({
+    name: "",
+    number: "",
+    date: currentYear + "-" + formattedCurrentMonth,
+    cvv: "",
+  });
+
+  const [side, setSide] = useState("front");
+  const [message, setMessage] = useState(
+    "Please enter you credit card information"
+  );
+  function handleSubmit(e) {
+    e.preventDefault();
+    setMessage("Thank you your custom");
+    setFormData({
+      name: "",
+      number: "",
+      cvv: "",
+    });
+    setSide("front");
+  }
+  function handleChange(e) {
+    const name = e.target.name;
+    let value = e.target.value;
+    if (name === "cvv" || name === "number") {
+      value = value.replace(/[^0-9.]/g, "").replace(/(\..*)\./g, "$1");
+    }
+    setFormData({ ...formData, [name]: value });
+    if (name === "cvv") {
+      setSide("back");
+      return;
+    }
+    setSide("front");
   }
 
   return (
     <>
       <div className="form-container">
-        <CreditCard />
+        <CreditCard formData={formData} side={side} />
         <form onSubmit={handleSubmit}>
           <div className="input-container">
             <label>
               Name on card
-              <input placeholder="Name" required />
+              <input
+                name="name"
+                value={formData.name}
+                onChange={handleChange}
+                placeholder="Name"
+                required
+              />
             </label>
           </div>
           <div className="input-container">
             <label>
               Card Number
               <input
+                name="number"
+                value={formData.number}
+                onChange={handleChange}
                 placeholder="0000 0000 0000 0000"
                 minLength={16}
                 maxLength={16}
@@ -31,12 +76,21 @@ function App() {
           <div className="supporting-inputs-container">
             <label>
               Expiry date
-              <input type="month" required />
+              <input
+                type="month"
+                required
+                name="date"
+                value={formData.date}
+                onChange={handleChange}
+              />
             </label>
             <label>
               CVV
               <input
                 id="cvv"
+                name="cvv"
+                value={formData.cvv}
+                onChange={handleChange}
                 placeholder="123"
                 minLength={3}
                 maxLength={3}
@@ -47,7 +101,7 @@ function App() {
           <div className="input-container">
             <input type="submit" />
           </div>
-          <p className="info-message"></p>
+          <p className="info-message">{message}</p>
         </form>
       </div>
     </>
--- src/components/CreditCard.jsx
+++ src/components/CreditCard.jsx
@@ -1,28 +1,53 @@
 import React from "react";
 
-const CreditCard = () => {
+const CreditCard = ({ formData, side }) => {
+  const startState = [
+    "X",
+    "X",
+    "X",
+    "X",
+    "X",
+    "X",
+    "X",
+    "X",
+    "X",
+    "X",
+    "X",
+    "X",
+    "X",
+    "X",
+    "X",
+    "X",
+  ];
   return (
     <div className="credit-card">
-      <div className="visa-front-side">
-        <div className="card-details-container card">
-          <div className="detail-container">
-            <p>Expiry:</p>
-            <h2>XXX</h2>
-          </div>
-          <div className="detail-container">
-            <p>Cardholder:</p>
-            <h2>XXXXX</h2>
-          </div>
-          <div className="number-display">
-            <h3>XXXXXXX</h3>
+      {side === "front" && (
+        <div className="visa-front-side">
+          <div className="card-details-container card">
+            <div className="detail-container">
+              <p>Expiry:</p>
+              <h2>{formData.date}</h2>
+            </div>
+            <div className="detail-container">
+              <p>Cardholder:</p>
+              <h2>{formData.name}</h2>
+            </div>
+            <div className="number-display">
+              {startState.map((item, index) => {
+                return <h3 key={index}>{formData.number[index] || item}</h3>;
+              })}
+            </div>
           </div>
         </div>
-      </div>
-      <div className="visa-back-side card">
-        <div id="cvv-display">
-          <p>XXX</p>
+      )}
+
+      {side === "back" && (
+        <div className="visa-back-side card">
+          <div id="cvv-display">
+            <p>{formData.cvv}</p>
+          </div>
         </div>
-      </div>
+      )}
     </div>
   );
 };
